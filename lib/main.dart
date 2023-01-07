ca9ca8deef2e962e2b90d7e6cc7355376b7903dd
Netesh5 <netesh.paudel@gmail.com>
2023-01-07 21:43:16 +0545

    Firebase Intialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:quicksummary/firebase_options.dart';
 
 import 'view/myapp.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
