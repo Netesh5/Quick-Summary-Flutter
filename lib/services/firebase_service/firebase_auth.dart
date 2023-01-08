f9d611e36d0a8ae40743ccbc3715066974f5f5bd
Netesh5 <netesh.paudel@gmail.com>
2023-01-08 18:55:06 +0545

    google auth implementation

--- lib/services/firebase_service/firebase_auth.dart
+++ lib/services/firebase_service/firebase_auth.dart
@@ -1,34 +1,54 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:quicksummary/resources/string_manager.dart';
 import 'package:quicksummary/utils/snackbar.dart';
 
 class AuthService {
+  User? user;
+  Stream<User?> get authState {
+    return auth.authStateChanges();
+  }
+
   FirebaseAuth auth = FirebaseAuth.instance;
   Future googleSignIn(BuildContext context) async {
-    GoogleSignIn signIn = GoogleSignIn();
-    GoogleSignInAccount? signInAccount =
-        await signIn.signIn().catchError((error) {
-      snackBar(error, context);
-    });
-    if (signInAccount != null) {
-      GoogleSignInAuthentication signInAuthentication =
-          await signInAccount.authentication;
-      OAuthCredential credential = GoogleAuthProvider.credential(
-          accessToken: signInAuthentication.accessToken,
-          idToken: signInAuthentication.idToken);
-      try {
-        UserCredential userCredential =
-            await auth.signInWithCredential(credential);
-        // ignore: use_build_context_synchronously
-        snackBar(StringManager.successfully, context);
-      } on FirebaseAuthException catch (e) {
-        // ignore: use_build_context_synchronously
-        snackBar(e.message!, context);
+    try {
+      GoogleSignIn signIn = GoogleSignIn();
+      GoogleSignInAccount? signInAccount =
+          await signIn.signIn().catchError((error) {
+        snackBar(error, context);
+      });
+      if (signInAccount != null) {
+        GoogleSignInAuthentication signInAuthentication =
+            await signInAccount.authentication;
+        OAuthCredential credential = GoogleAuthProvider.credential(
+            accessToken: signInAuthentication.accessToken,
+            idToken: signInAuthentication.idToken);
+        try {
+          UserCredential userCredential =
+              await auth.signInWithCredential(credential);
+          user = userCredential.user;
+          // ignore: use_build_context_synchronously
+          snackBar(StringManager.successfully, context);
+        } on FirebaseAuthException catch (e) {
+          // ignore: use_build_context_synchronously
+          snackBar(e.message!, context);
+        }
+      } else {
+        return null;
       }
-    } else {
-      return null;
+    } on PlatformException catch (e) {
+      debugPrint(e.message);
+    }
+    return user;
+  }
+
+  Future googleLogOut(BuildContext context) async {
+    try {
+      return await auth.signOut();
+    } on FirebaseException catch (e) {
+      snackBar(e.message!, context);
     }
   }
 }
